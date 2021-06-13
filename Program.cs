f88298c52a4735b4e74e2823e397f9f6fff5fe57
DESKTOP-KC8155E\Lenovo <EasterEggs1230@gmail.com>
2021-06-13 16:51:13 +0700

    63120501056 Test No.5

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Test_No._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int NumofItem = int.Parse(Console.ReadLine());
+            string[] Item = new string [NumofItem];
+            string[] ItemType = new string[NumofItem];
+
+            int slot = 0;
+            for (int i = 1; i <= NumofItem; i++)
+            {
+                Console.Write("Item: ");
+                Item [slot] = Console.ReadLine();
+                Console.Write("Type: ");
+                ItemType [slot] = Console.ReadLine();
+                slot++;
+            }
+
+            while (true)
+            {
+                Console.Write("Input Mode: ");
+                string Mode = Console.ReadLine();
+
+
+                if (Mode == "Showall")
+                {
+                    int slot1 = 0;
+                    for (int j = 1; j <= NumofItem; j++)
+                    {
+                        Console.WriteLine(Item[slot1]);
+                        Console.WriteLine(ItemType[slot1]);
+                        Console.WriteLine();
+                        slot1++;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("End;");
+                    return;
+                }
+            }
+        }
+    }
+}
